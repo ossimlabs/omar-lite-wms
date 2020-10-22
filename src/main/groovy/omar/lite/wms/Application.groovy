c4f34075cc4539d3a5f04590471dbfec59450b07
sbortman <scott.bortman@gmail.com>
2020-10-22 13:08:38 -0400

    added swagger deps and configs

--- src/main/groovy/omar/lite/wms/Application.groovy
+++ src/main/groovy/omar/lite/wms/Application.groovy
@@ -2,10 +2,18 @@ package omar.lite.wms
 
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "omar-wms-lite",
+        version = "0.0"
+    )
+)
 @CompileStatic
 class Application {
-    static void main(String[] args) {
-        Micronaut.run(Application, args)
-    }
+  static void main( String[] args ) {
+    Micronaut.run( Application, args )
+  }
 }
